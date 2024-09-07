8f3a3285014865fedef2e8c79fddfd761fac455b
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-08 00:40:21 +0900

    usersテーブル・モデルを作成

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  def self.find_or_create_from_discord_info(auth_info)
+    User.find_or_create_by(discord_uid: auth_info.uid) do |user|
+      user.name = auth_info.info.name
+      user.avatar_url = auth_info.info.image
+    end
+  end
+end
