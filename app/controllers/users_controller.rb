94874f1b4519a70875702a61d266e7e94a41e5b8
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-17 16:35:41 +0900

    showとeditのアクションとviewを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by(params[:user_id])
+  end
+
+  def edit
+    @user = User.find_by(params[:user_id])
+  end
 end
