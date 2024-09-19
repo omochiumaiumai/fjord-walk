3c1a6cc96ce3565f7e51d89ac632fc0969696344
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-19 19:10:27 +0900

    ユーザー名の変更機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,34 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update]
+
   def show
-    @user = User.find_by(params[:user_id])
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find_by(params[:user_id])
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'ユーザー名が更新されました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+    return unless @user.nil?
+
+    redirect_to root_path, alert: 'ユーザーが見つかりません。'
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
