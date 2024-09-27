943a3ddc2760e9b2071ee870ae5eeb23088d27ae
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-28 02:56:24 +0900

    schemaを更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_17_085504) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_22_065715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_085504) do
     t.index ["user_id"], name: "index_event_participants_on_user_id"
   end
 
+  create_table "event_repeat_rules", force: :cascade do |t|
+    t.bigint "event_id"
+    t.integer "frequency", null: false
+    t.integer "day_of_the_week", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_event_repeat_rules_on_event_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -44,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_085504) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.boolean "hold_national_holiday", default: false, null: false
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -69,6 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_17_085504) do
   add_foreign_key "attendances", "users"
   add_foreign_key "event_participants", "events"
   add_foreign_key "event_participants", "users"
+  add_foreign_key "event_repeat_rules", "events"
   add_foreign_key "events", "users"
   add_foreign_key "talk_themes", "events"
   add_foreign_key "talk_themes", "users"
