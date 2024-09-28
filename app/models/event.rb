c79e0946484aed91983709aaae1dd673813d32b3
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-28 09:52:09 +0900

    リレーションの誤字修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  has_many :event_participants, dependent: :estrict_with_errordes
-  has_many :attendances, dependent: :estrict_with_errordes
+  has_many :event_participants, dependent: :restrict_with_error
+  has_many :attendances, dependent: :restrict_with_error
   has_many :talk_themes, dependent: :destroy
   belongs_to :users
 end
