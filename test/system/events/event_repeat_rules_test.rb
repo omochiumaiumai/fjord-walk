bfd25f9c1127b3188f46a4264a5e43040e9d50dd
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-29 02:24:50 +0900

    各システムテストを作成

---
+++ test/system/events/event_repeat_rules_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class EventRepeatRulesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit event_repeat_rules_url
+  #
+  #   assert_selector "h1", text: "EventRepeatRules"
+  # end
+end
