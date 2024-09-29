a38a0e9e40ad76b895c8df8f719c9ca61a6401ff
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-09-29 12:45:53 +0900

    event_repeat_rulesのリレーションを設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,9 @@
 
 class Event < ApplicationRecord
   has_many :event_participants, dependent: :restrict_with_error
+  has_many :event_repeat_rules, dependent: :destroy
   has_many :attendances, dependent: :restrict_with_error
   has_many :talk_themes, dependent: :destroy
   belongs_to :users
+
 end
