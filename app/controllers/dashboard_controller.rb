6236ded1b483ebac5c04099c3f456cf4e9c6931d
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-10-06 19:19:23 +0900

    ユーザーの出席情報を取得した変数を追加

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,11 @@ class DashboardController < ApplicationController
   before_action :set_beginning_of_week
   def show
     @user = User.find(params[:user_id])
+    @attendances = @user.attendances.pluck(:attended_on)
+    @attended_events = @user.attendances.includes(:event).where.not(attended_on: nil)
+    @attended_users = @attended_events.map(&:event).flat_map do |event|
+      event.attendances.includes(:user).where.not(attended_on: nil).map(&:user)
+    end.uniq
   end
 
   private
