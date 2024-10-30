cc8ee2ed48e0f10a6a7f281a496a9bc59019c5bf
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-10-31 01:37:06 +0900

    カレンダーを日曜始まりに変更

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
+  before_action :set_beginning_of_week
   def show
     @user = User.find(params[:user_id])
+  end
+
+  private
+
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
   end
 end
