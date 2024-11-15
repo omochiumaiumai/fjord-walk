7688bc0e45cba0e17cb5e8243e4f97a6ba6f4398
omochiumaiumai <shu.nakazato0419@gmail.com>
2024-11-15 16:41:10 +0900

    重複するjqueryライブラリを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ require('turbolinks').start()
 require('@rails/activestorage').start()
 require('channels')
 
-require("jquery")
 require("@nathanvda/cocoon")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
